8eeddc9db43bfac1339c3e4b0d1abb21deaaafa5
kcmuths <kcmuths@ccs.neu.edu>
2016-03-15 14:51:12 -0400

    webservice reqs(some bugs there)

---
+++ public/assignment/server/services/user.service.server.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = function(app, model){
+    app.post('/api/assignment/user', function(req, res){
+        res.json(model.Create(req.body));
+    });
+    app.get('/api/assignment/user', function(req,res){
+        var username = req.param('username');
+        var password = req.param('password');
+
+        if(username == null && password == null)
+        {
+            res.json(model.FindAll());
+            return;
+        }
+        if(password == null){
+            res.json(model.FindUserByUsername(username));
+            return;
+        }
+        res.json(model.FindUserByCredentials({
+            username: username,
+            password: password
+        }));
+    });
+    app.get('api/assignment/user/:id', function(req, res){
+        res.json(model.FindById(req.params._id));
+    });
+    app.put('api/assignment/user/:id', function(req, res){
+        res.json(model.Update(req.params._id, req.body));
+    });
+    app.delete('/api/assignment/user/:id', function(req, res){
+        res.json(model.Delete(req.params._id));
+    });
+};
