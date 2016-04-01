ba6f338c7631d4c0aaf664bcf1dcfa01543a0e56
kcmuths <kcmuths@ccs.neu.edu>
2016-04-01 13:01:49 -0400

    server changes

--- server.js
+++ server.js
@@ -12,23 +12,20 @@ app.listen(port, ipaddress);
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var multer = require('multer');
 var mongoose = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
 
 app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(multer());
 
-if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
+//var db = mongoose.connect(connectionString);
+require("./public/assignment/server/app.js")(app);
 
-var db = mongoose.connect(connectionString);
-require("./public/assignment/server/app.js")(app, mongoose, db);
+require("./public/Project/server/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
