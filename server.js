00b6945e50037d5df93cfd5a3c43ad87cd9fe5d5
kcmuths <kcmuths@ccs.neu.edu>
2016-04-11 18:58:50 -0400

    Openshift deployment error

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-var connectionString = 'mongodb://localhost/webdevsp16';
+var connectionString = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017/webdevsp16';
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+/*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
-}
+} */
 
 var db = mongoose.connect(connectionString);
 require("./public/Experiments/Threads/models/Posts.js");
