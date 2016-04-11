5f4d2fcd52e8e55fd1ebeab84e7e170725b91dbc
kcmuths <kcmuths@ccs.neu.edu>
2016-04-11 15:11:41 -0400

    Experimenting Threader news project

---
+++ public/Experiments/Threads/app.js
@@ -0,0 +1,62 @@
+var app = angular.module('Threads', ['ui.router']);
+app.factory('posts',[function(){
+    var o = {
+        posts:[]
+    };
+    return o;
+}]);
+app.controller('MainCtrl',['$scope','posts',function($scope, posts){
+    $scope.test= 'Hello World'
+    $scope.posts = posts.posts;
+        /*[
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 9},
+        {title: 'post 5', upvotes: 4}
+    ]; */
+    $scope.addPost = function(){
+        if(!$scope.title || $scope.title === '')
+        {
+            return;
+        }
+        $scope.posts.push({title: $scope.title, link:$scope.link, upvotes: 0,
+        comments: [
+            {author:'Grace', body:'Slick', upvotes: 0},
+            {author:'Alice', body:'so and so', upvotes: 0}
+        ]});
+        $scope.title = '';
+        $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+    };
+}]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl:'/posts.html',
+            controller: 'PostsCtrl'
+        });
+        $urlRouterProvider.otherwise('home');
+    }
+]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+        $scope.post = posts.posts[$stateParams.id];
+
+    }
+]);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*var express = require('express');
+var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
@@ -7,14 +7,14 @@ app.get('/hello', function(req, res){
     res.send('hello world');
 });
 app.listen(port, ipaddress);
-*/
 
-var express = require('express');
+
+/*var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
-var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
+var connectionString = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -31,3 +31,5 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.listen(port, ipaddress);
+*/
+
